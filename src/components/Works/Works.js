76da363216c8ba9cae110c32663f4314868935d0
Jeremiah Samuel <livinalt@gmail.com>
2024-03-22 11:04:53 +0100

    added project

--- src/components/Works/Works.js
+++ src/components/Works/Works.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import './Works.css';
-import Portfolio1 from '../../assets/portfolio-1.png';
-import Portfolio2 from '../../assets/portfolio-2.png';
-import Portfolio3 from '../../assets/portfolio-3.png';
-import Portfolio4 from '../../assets/portfolio-4.png';
-import Portfolio5 from '../../assets/portfolio-5.png';
-import Portfolio6 from '../../assets/portfolio-6.png';
+import contact_app from '../../assets/contact-app.png';
+import counterApp from '../../assets/counter-app.png';
+import multiStepFormApp from '../../assets/multi-step-form.png';
+import ecommerceApp from '../../assets/ecommerce-app.png';
+import easybankApp from '../../assets/easybank-app.png';
+import quizzApp from '../../assets/quiz-app.png';
 
 const Works = () => {
   return (
@@ -13,12 +13,12 @@ const Works = () => {
        <h2 className='worksTitle'>My Portfolio</h2>
        <span className='worksDesc'>I take pride in paying attention to the smallest details and </span>
        <div className='worksImgs'>
-            <img src ={Portfolio1} alt ="" className='worksImg' />
-            <img src ={Portfolio2} alt ="" className='worksImg' />
-            <img src ={Portfolio3} alt ="" className='worksImg' />
-            <img src ={Portfolio4} alt ="" className='worksImg' />
-            <img src ={Portfolio5} alt ="" className='worksImg' />
-            <img src ={Portfolio6} alt ="" className='worksImg' />
+            <a href ="https://contactly-wheat.vercel.app/"><img src ={contact_app} alt ="" className='worksImg' /></a>
+            <a href = "https://passenger-counter-livinalt.vercel.app/" ><img src ={counterApp} alt ="" className='worksImg' /></a>
+            <a href = "https://form-project-seven.vercel.app/" ><img src ={multiStepFormApp} alt ="" className='worksImg' /></a>
+            <a href = "https://e-com-livinalt.vercel.app/" ><img src ={ecommerceApp} alt ="" className='worksImg' /></a>
+            <a href = "https://easybank-landing-page-livinalt.vercel.app/" ><img src ={easybankApp} alt ="" className='worksImg' /></a>
+            <a href = "https://quizz-app-jade.vercel.app/"><img src ={quizzApp} alt ="" className='worksImg' /></a>
         </div>   
 
         <button className='workBtn'>See More</button>
